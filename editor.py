1a33155e673c01174ad09dc872e16fe12bf6745c
Cătălin Badea <catalin.badea392@gmail.com>
2013-04-21 02:02:30 +0300

    Replace tabs with 4 spaces. modify import from PyQt4.

--- editor.py
+++ editor.py
@@ -1,29 +1,31 @@
 from highlight import PythonHighlighter
-from PyQt4 import QtGui, QtCore, Qt
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
 import sys
 
-class Editor(QtGui.QWidget):
-    on_execute = QtCore.pyqtSignal(dict)
+class Editor(QWidget):
+    on_execute = pyqtSignal(dict)
 
     def __init__(self):
         super(Editor, self).__init__()
 
-        self.setLayout(QtGui.QVBoxLayout())
+        self.setLayout(QVBoxLayout())
 
-        label = QtGui.QLabel("Code:")
+        label = QLabel("Code:")
 
         #textedit and console output
-        self.text_splitter = QtGui.QSplitter(self)
+        self.text_splitter = QSplitter(self)
         self.text_splitter.setOrientation(2) # Qt.Vertical = 2.
 
-        self.text_area = QtGui.QPlainTextEdit(self.text_splitter)
-        self.console_output = QtGui.QPlainTextEdit(self.text_splitter)
+        self.text_area = QPlainTextEdit(self.text_splitter)
+        self.console_output = QPlainTextEdit(self.text_splitter)
 
-        vertical_stretch = QtGui.QSizePolicy()
+        vertical_stretch = QSizePolicy()
         vertical_stretch.setVerticalStretch(185)
         self.text_area.setSizePolicy(vertical_stretch)
 
-        vertical_stretch_console = QtGui.QSizePolicy()
+        vertical_stretch_console = QSizePolicy()
         vertical_stretch_console.setVerticalStretch(2)
         self.console_output.setSizePolicy(vertical_stretch_console)
         self.console_output.setMinimumHeight(60)
@@ -35,8 +37,8 @@ class Editor(QtGui.QWidget):
         self.text_splitter.addWidget(self.console_output)
 
         # buttons
-        run_button = QtGui.QPushButton("Run")
-        self.reload_button = QtGui.QPushButton("Reload")
+        run_button = QPushButton("Run")
+        self.reload_button = QPushButton("Reload")
 
         self.layout().addWidget(label)
         self.layout().addWidget(self.text_splitter)
@@ -45,6 +47,15 @@ class Editor(QtGui.QWidget):
 
         # connect run button
         run_button.clicked.connect(self.runPressed)
+        self.text_area.keyPressEvent = self.key_press
+
+    def key_press(self, event):
+        key = event.key()
+        if key == Qt.Key_Tab:
+            cursor = self.text_area.textCursor()
+            cursor.insertText("    ")
+        else:
+            QPlainTextEdit.keyPressEvent(self.text_area, event)
 
     def runPressed(self):
         print "run pressed."
